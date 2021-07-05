7b9c3a9c43d2dbbdec0c5d462738c72675ef15cd
Exeteres <exeteres@yandex.ru>
2021-07-05 18:15:33 +0700

    feat: Implement cacheAndNetwork post fetching policy

--- lib/features/posts/domain/services/post_repository.dart
+++ lib/features/posts/domain/services/post_repository.dart
@@ -3,7 +3,7 @@ import 'package:shetter_app/core/domain/domain.dart';
 import 'package:shetter_app/features/posts/domain/domain.dart';
 
 abstract class PostRepository {
-  Future<Either<Failure, Connection<Post>>> getPosts({
+  Stream<Either<Failure, Connection<Post>>> getPosts({
     required int pageSize,
     String? after,
   });
