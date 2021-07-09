a8b6a0c0aff12c4cad66b37f777398afd72fff9c
cirnok <laim0n@yandex.ru>
2021-07-09 15:35:04 +0300

    fix: Add auth bloc builder

--- lib/features/auth/presentation/widgets/auth_fragment.dart
+++ lib/features/auth/presentation/widgets/auth_fragment.dart
@@ -5,9 +5,9 @@ class AuthFragment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AuthFragmentBloc, AuthFragmentState>(
+    return BlocBuilder<AuthBloc, AuthState>(
       builder: (context, state) {
-        if (state is AuthFragmentStateAuthenticated) return Container();
+        if (state is AuthStateAuthenticated) return Container();
 
         return UCard.outline(
           style: UCardStyle(
@@ -15,47 +15,51 @@ class AuthFragment extends StatelessWidget {
               horizontal: DesignConstants.paddingValue,
             ).copyWith(top: DesignConstants.paddingMiniValue),
           ),
-          child: UFrameLoader(
-            state: state.maybeMap(
-              loading: (_) => UFrameLoaderState.loading(),
-              orElse: () => UFrameLoaderState.initial(),
-            ),
-            child: Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              crossAxisAlignment: WrapCrossAlignment.center,
-              alignment: WrapAlignment.end,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: BlocBuilder<AuthFragmentBloc, AuthFragmentState>(
+            builder: (context, state) {
+              return UFrameLoader(
+                state: state.maybeMap(
+                  loading: (_) => UFrameLoaderState.loading(),
+                  orElse: () => UFrameLoaderState.initial(),
+                ),
+                child: Wrap(
+                  spacing: 10,
+                  runSpacing: 10,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  alignment: WrapAlignment.end,
                   children: [
-                    UTextField(
-                      hintText: Strings.username.get(),
-                      controller: state.usernameController,
-                      icon: Icon(Icons.ac_unit),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        UTextField(
+                          hintText: Strings.username.get(),
+                          controller: state.usernameController,
+                          icon: Icon(Icons.ac_unit),
+                        ),
+                        SizedBox(height: 10),
+                        UTextField(
+                          hintText: Strings.password.get(),
+                          controller: state.passwordController,
+                          isPassword: true,
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 10),
-                    UTextField(
-                      hintText: Strings.password.get(),
-                      controller: state.passwordController,
-                      isPassword: true,
+                    UButton.outline(
+                      onPressed: context.read<AuthFragmentBloc>().auth,
+                      child: Text(
+                        Strings.login.get(),
+                      ),
+                    ),
+                    UButton(
+                      onPressed: context.read<AuthFragmentBloc>().register,
+                      child: Text(
+                        Strings.signin.get(),
+                      ),
                     ),
                   ],
                 ),
-                UButton.outline(
-                  onPressed: context.read<AuthFragmentBloc>().auth,
-                  child: Text(
-                    Strings.login.get(),
-                  ),
-                ),
-                UButton(
-                  onPressed: context.read<AuthFragmentBloc>().register,
-                  child: Text(
-                    Strings.signin.get(),
-                  ),
-                ),
-              ],
-            ),
+              );
+            },
           ),
         );
       },
