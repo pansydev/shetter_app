2b7c2d7e15925bc945d5b4d6565a63f27dcbb418
Exeteres <exeteres@yandex.ru>
2021-07-18 06:46:39 +0700

    refactor: Disable "one_member_abstract" analyzer rule

--- lib/core/infrastructure/services/auth_link_factory.dart
+++ lib/core/infrastructure/services/auth_link_factory.dart
@@ -1,6 +1,5 @@
 import 'package:graphql/client.dart';
 
-// ignore: one_member_abstracts
 abstract class AuthLinkFactory {
   Link createAuthLink();
 }
--- lib/features/auth/infrastructure/services/refresh_manager.dart
+++ lib/features/auth/infrastructure/services/refresh_manager.dart
@@ -1,7 +1,6 @@
 import 'package:shetter_app/features/auth/domain/domain.dart';
 import 'package:shetter_app/features/auth/infrastructure/infrastructure.dart';
 
-// ignore: one_member_abstracts
 abstract class RefreshManager {
   Future<Option<Failure>> ensureRefreshed({bool force});
 }
