06179bd1255de65e5573de13daabc9db82def994
Exeteres <exeteres@yandex.ru>
2021-08-16 15:02:32 +0700

    feat: Add pansy_ui localization

--- lib/core/presentation/exports.dart
+++ lib/core/presentation/exports.dart
@@ -2,6 +2,5 @@ export 'package:simple_animations/simple_animations.dart';
 export 'package:intl/intl.dart' hide TextDirection;
 export 'package:provider/provider.dart';
 
-export 'package:pansy_ui/pansy_ui.dart';
 export 'package:pansy_accounts/presentation/presentation.dart';
 export 'package:pansy_core/presentation/presentation.dart';
